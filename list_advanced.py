df3bfd4b7bade21de9672736e2b4f98c190c828c
Anastasiia Valdenmaiier <anastasiia.kostashenko@gmail.com>
2017-08-16 21:59:50 +0200

    Update list_advanced
    add example of lambda function

--- list_advanced.py
+++ list_advanced.py
@@ -30,3 +30,6 @@ print (filter(lambda x: x % 3 == 0, my_list))
 
 languages = ["HTML", "JavaScript", "Python", "Ruby"]
 print (filter(lambda x: x=="Python", languages))
+
+squares = [i*i for i in range(1,11)]
+print (filter(lambda x: x>=30 and x <= 70, squares))
