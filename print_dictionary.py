146321944da85ddd465799e4cc463cc00de262b7
Anastasiia Valdenmaiier <anastasiia.kostashenko@gmail.com>
2017-08-16 21:00:58 +0200

    Update print_dictionary
    Add more methods to print dictionary

--- print_dictionary.py
+++ print_dictionary.py
@@ -16,6 +16,24 @@ for key in prices:
     print ("Prices: %s" % prices[key])
     print ("Stock: %s" % stock[key])
   
+  
+# line 29 print items such a tuples
+# line 30 print keys
+# line 31 print values
+# line 34 print keys and values each in a line
+my_dict = {
+  'Mom' : 64,
+  'papa' : 113,
+  'weight' : True
+}
+print (my_dict.items())
+print (my_dict.keys())
+print (my_dict.values())
+
+for key in my_dict:
+  print (key, my_dict[key])
+  
+  
 #  total value of inventory
 total = 0
 for key in prices:
