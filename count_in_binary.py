229aab137a98742534970aa31c48e21147f2a20d
Anastasiia Valdenmaiier <anastasiia.kostashenko@gmail.com>
2017-08-18 12:38:05 +0200

    create count_in_binary
    Numbers in binary system, bin() function, print decimal equivalent

---
+++ count_in_binary.py
@@ -0,0 +1,41 @@
+print (0b1,)    #1
+print (0b10,)   #2
+print (0b11,)   #3
+print (0b100,)  #4
+print (0b101,)  #5
+print (0b110,)  #6
+print (0b111)   #7
+print ("******")
+print (0b1 + 0b11)
+print (0b11 * 0b11)
+
+
+#number 1-12 in binary system
+one = 0b1
+two = 0b10
+three = 0b11
+four = 0b100
+five = 0b101
+six = 0b110
+seven = 0b111
+eight = 0b1000
+nine = 0b1001
+ten = 0b1010
+eleven = 0b1011
+twelve = 0b1100
+
+
+#bin() function. Also oct(), hex()
+print (bin(1))
+print (bin(2))
+print (bin(3))
+print (bin(4))
+print (bin(5))
+print (oct(1))
+print (oct(5))
+print (oct(7))
+print (oct(8))
+
+
+# Print out the decimal equivalent of the binary
+print (int("0b11001001",2))
