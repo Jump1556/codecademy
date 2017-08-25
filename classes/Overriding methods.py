f9a44e1703f7ae9618ea5e946cb3cc684f7de9ca
Anastasiia Valdenmaiier <anastasiia.kostashenko@gmail.com>
2017-08-25 11:54:26 +0200

    Initializing a class

--- classes/Overriding methods.py
+++ classes/Overriding methods.py
@@ -1,4 +1,13 @@
 # Define a new class named "Car"
 class Car (object):
-  pass
-  condition = "new"     # member variables
+   def __init__(self, model, color, mpg):
+    # in order to assign a member variable,use dot notation self.new_variable=new_variable
+    self.model = model
+    self.color = color
+    self.mpg = mpg
+    condition = "new"     # member variables
+my_car = Car("DeLorean", "silver", 88)
+#an object(instance of class)
+model = "DeLorean"
+color = "silver"
+mpg = 88
