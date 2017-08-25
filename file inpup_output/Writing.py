8a43a314ada62f0ab04983b11233c98e840d1628
Anastasiia Valdenmaiier <anastasiia.kostashenko@gmail.com>
2017-08-25 13:09:31 +0200

    Udpate Writing
    Write my_list to output.txt

--- file inpup_output/Writing.py
+++ file inpup_output/Writing.py
@@ -1,2 +1,7 @@
-my_file = open("output.txt", "r+")
-# file will allow to read and write to it
+my_list = [i ** 2 for i in range(1, 11)]
+
+my_file = open("output.txt", "w")
+
+for i in my_list:                # Iterate over my_list to get each value.
+  my_file.write(str(i) + "\n")   # add a newline 
+my_file.close()                  # important to close file!
